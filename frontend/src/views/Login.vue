d3bf7ce87c0b1077da9fbb82a2f441ac69168fd7
batsWTR <baptiste.wentzler@wanadoo.fr>
2021-06-15 17:35:06 +0200

    ajout html login et signup

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,13 +1,50 @@
 <template>
-  <h1>login</h1>
-  <div>
-    <div>
-        <label for="loginUsername">Pseudo </label>
-        <input type="text" id="loginUsername" v-model="loginUsername" required/>
-    </div>
-    <div>
-        <label for="loginPassword">Mot de passe </label>
-        <input type="text" id="loginPassword" v-model="loginPassword" required/>
-    </div>
-  </div>
+  <form class="form-signin">
+      <img class="mb-4" src="../assets/icon.svg" alt="" width="72" height="72">
+      <h1 class="h3 mb-3 font-weight-normal">Connection</h1>
+      <label for="inputEmail" class="sr-only">Adresse mail</label>
+      <input type="email" id="inputEmail" class="form-control" placeholder="Adresse mail" required="" autofocus="" v-model='email'>
+      <label for="inputPassword" class="sr-only">Mot de passe</label>
+      <input type="password" id="inputPassword" class="form-control" placeholder="Mot de passe" required="" v-model='motdepasse'>
+      <div class="checkbox mb-3">
+      </div>
+      <button class="btn btn-lg btn-primary btn-block" type="submit" @click="signin">Se connecter</button>
+  </form>
+    <br/>
+    <h2>{{ msg }}</h2>
+
 </template>
+
+
+<script>
+
+export default {
+  name: 'login',
+  data() {
+    return{
+      email: '',
+      motdepasse: '',
+      msg: ''
+    }
+  },
+  methods: {
+    signin(){
+      if(this.email != '' && this.motdepasse != ''){
+        this.msg = 'envoie au backend'
+      }
+    }
+  }
+}
+</script>
+
+
+
+<style scoped>
+.form-signin {
+    width: 100%;
+    max-width: 330px;
+    padding: 15px;
+    margin: 0 auto;
+    text-align: center;
+}
+</style>
--- frontend/src/views/Signup.vue
+++ frontend/src/views/Signup.vue
@@ -1,3 +1,49 @@
 <template>
-    <h1>signup</h1>
+      <form class="form-signup">
+      <img class="mb-4" src="../assets/icon.svg" alt="" width="72" height="72">
+      <h1 class="h3 mb-3 font-weight-normal">Inscription</h1>
+      <label for="pseudo" class="sr-only">Pseudo</label>
+      <input type="text" id="pseudo" class="form-control" placeholder="Pseudo" required="" autofocus="" v-model='pseudo'>
+      <label for="inputEmail" class="sr-only">Adresse mail</label>
+      <input type="email" id="inputEmail" class="form-control" placeholder="Addresse mail" required="" autofocus="" v-model='email'>
+      <label for="inputPassword" class="sr-only">Mot de passe</label>
+      <input type="password" id="inputPassword" class="form-control" placeholder="Mot de passe" required="" v-model='motdepasse'>
+      <div class="checkbox mb-3">
+      </div>
+      <button class="btn btn-lg btn-primary btn-block" type="submit" @click="signup">S'inscrire</button>
+  </form>
+  <br/>
+  <h2>{{ msg }}</h2>
 </template>
+
+<script>
+export default {
+  name: 'signup',
+  data() {
+    return{
+        pseudo: '',
+        email: '',
+        motdepasse: '',
+        msg: ''
+    }
+  },
+  methods: {
+    signup(){
+      if(this.pseudo != '' && this.email != '' && this.motdepasse != ''){
+        this.msg = 'envoie au backend'
+      }
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+.form-signup {
+    width: 100%;
+    max-width: 330px;
+    padding: 15px;
+    margin: 0 auto;
+    text-align: center;
+}
+</style>
